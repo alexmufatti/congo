a752020bf59323a260f4ce45921ef900f5228462
Alex Mufatti <me@alexmufatti.it>
2024-03-15 21:25:07 +0100

    colorschema

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -3,12 +3,13 @@ function css(name) {
 }
 
 Chart.defaults.font.size = 14;
-Chart.defaults.plugins.colors.enabled = false;
+Chart.defaults.plugins.colors.enabled = true;
 Chart.defaults.backgroundColor = css("--color-primary-300");
 Chart.defaults.elements.point.borderColor = css("--color-primary-400");
 Chart.defaults.elements.bar.borderColor = css("--color-primary-500");
 Chart.defaults.elements.bar.borderWidth = 1;
 Chart.defaults.elements.line.borderColor = css("--color-primary-400");
+Chart.defaults.elements.line.borderWidth = 1;
 Chart.defaults.elements.arc.backgroundColor = css("--color-primary-200");
 Chart.defaults.elements.arc.borderColor = css("--color-primary-500");
 Chart.defaults.elements.arc.borderWidth = 1;
